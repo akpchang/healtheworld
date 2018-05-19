149222c487f05ec82d0e7c4e1c7370013c8ece2b
Austin Keola Chang <akchang@student.fullsail.edu>
2018-05-19 13:52:13 -1000

    Initial Commit

---
+++ heal_the_world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  heal_the_world
+//
+//  Created by Austin Keola Pascal Chang on 5/19/18.
+//  Copyright © 2018 Austin Keola Pascal Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
